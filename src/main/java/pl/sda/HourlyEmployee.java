0c6217247babf18aea551dc3485d88f8e18e848e
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-01-10 19:54:53 +0100

    implements methods in HourlyEmployee nd WorkingDay

--- src/main/java/pl/sda/HourlyEmployee.java
+++ src/main/java/pl/sda/HourlyEmployee.java
@@ -3,9 +3,11 @@ package pl.sda;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class HourlyEmployee implements Payable {
@@ -21,17 +23,70 @@ public class HourlyEmployee implements Payable {
         this.workingDays = new ArrayList<>();
     }
 
-    public void addWorkingDay(WorkingDay workingDay){
+    public void addWorkingDay(WorkingDay workingDay) {
         workingDays.add(workingDay);
     }
 
     @Override
     public boolean isPaymentDay(LocalDate day) {
-        return false;
+        return day.getDayOfWeek() == DayOfWeek.FRIDAY;
     }
 
     @Override
     public BigDecimal calculatePayment(LocalDate day) {
-        return null;
+        if (isPaymentDay(day)) {
+            /*findMonday;
+            searchDaysBetweenMondayAndFriday;
+            wyplata dla jednego dnia;
+            wyplata dla wszystkich dni*/
+            LocalDate monday = findMonday(day);
+            List<WorkingDay> weekWorkingDays = findWorkingDays(monday, day);
+            return calculatePayment(weekWorkingDays);
+        } else {
+            return BigDecimal.ZERO;
+        }
+    }
+
+    private List<WorkingDay> findWorkingDays(LocalDate fromDay, LocalDate toDay) {
+        /*List<WorkingDay> workingDays = new ArrayList<>();
+        for (WorkingDay workingDay : this.workingDays) {
+            if (workingDay.isBetween(monday, day)) {
+                workingDays.add(workingDay);
+            }
+        }
+        return workingDays;*/
+        return workingDays.stream()
+                .filter(workingDay -> workingDay.isBetween(fromDay, toDay))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Znajduje poniedziałek dla danego piątku
+     *
+     * @param day piątek
+     * @return pasujący poniedziałek
+     */
+    private LocalDate findMonday(LocalDate day) {
+        if (day.getDayOfWeek() != DayOfWeek.FRIDAY) {
+            throw new IllegalStateException("Metoda findMonday() musi być wywołana dla piątku.");
+        }
+        return day.minusDays(4);
+    }
+
+    private BigDecimal calculatePayment(List<WorkingDay> weekWorkingDays) {
+        BigDecimal payment = BigDecimal.ZERO;
+        for (WorkingDay workingDay : weekWorkingDays) {
+            payment = payment.add(calculatePayment(workingDay));
+        }
+        return payment;
+    }
+
+    private BigDecimal calculatePayment(WorkingDay workingDay) {
+        int overHours = workingDay.getHours() - WORKING_HOURS;
+        int normalHours = workingDay.getHours() - overHours;
+        BigDecimal payment = BigDecimal.ZERO;
+        payment = payment.add(new BigDecimal(normalHours).multiply(hourlyRate));
+        payment = payment.add(new BigDecimal(overHours).multiply(hourlyRate.multiply(OVERHOURS_RATE)));
+        return payment;
     }
 }
--- src/main/java/pl/sda/WorkingDay.java
+++ src/main/java/pl/sda/WorkingDay.java
@@ -10,4 +10,21 @@ public class WorkingDay {
     final private LocalDate date;
     final private int hours;
 
+    /**
+     * Metoda sprawdza, czy workind day znajduje się w podanym przedziale (włącznie)
+     *
+     * @param fromDate początek przedziału
+     * @param toDate   koniec przedziału
+     * @return true jeżeli working day znajduje się w podanym okresie
+     */
+    public boolean isBetween(LocalDate fromDate, LocalDate toDate) {
+        boolean result = false;
+        if ((date.isAfter(fromDate) || date.isEqual(fromDate))
+                && date.isBefore(toDate) || date.isEqual(toDate)) {
+            result = true;
+        }
+        return result;
+
+        //return fromDate.compareTo(date) <= 0 && date.compareTo(toDate) <= 0;
+    }
 }
