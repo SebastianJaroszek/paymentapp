5e02c46171fa016126d9c811132e4540b3f8d2fe
SebastianJaroszek <sebastian_jaroszek@emstories.pl>
2018-01-11 20:24:12 +0100

    unit tests for CommisionalEmployee

--- src/test/java/pl/sda/CommisionalEmployeeTest.java
+++ src/test/java/pl/sda/CommisionalEmployeeTest.java
@@ -1,31 +1,50 @@
 package pl.sda;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CommisionalEmployeeTest {
 
+    private static final LocalDate SECOND_FRIDAY = LocalDate.of(2018, 1, 12);
+    private CommisionalEmployee commisionalEmployee;
+
+    @BeforeEach
+    void setup(){
+        commisionalEmployee = new CommisionalEmployee(new BigDecimal(2000));
+    }
+
     @Test
     void isPaymentDayShouldReturnTrueForEverySecondFriday(){
-
+        assertTrue(commisionalEmployee.isPaymentDay(SECOND_FRIDAY));
+        assertTrue(commisionalEmployee.isPaymentDay(SECOND_FRIDAY.plusDays(14)));
+        assertTrue(commisionalEmployee.isPaymentDay(SECOND_FRIDAY.plusDays(28)));
     }
 
     @Test
     void isPaymentDayShouldReturnFalseForNonFriday(){
-
+        assertFalse(commisionalEmployee.isPaymentDay(SECOND_FRIDAY.minusDays(1)));
     }
 
+    //piątek ale nie co drugi
     @Test
     void isPaymentDayShouldReturnFalseForNotEverySecondFriday(){
-
+        assertFalse(commisionalEmployee.isPaymentDay(SECOND_FRIDAY.minusDays(7)));
+        assertFalse(commisionalEmployee.isPaymentDay(SECOND_FRIDAY.plusDays(7)));
     }
 
     @Test
     void isPaymentDayShouldReturnTrueForLastWorkingDay(){
-
+        assertTrue(commisionalEmployee.isPaymentDay(LocalDate.of(2018, 12, 31)));
     }
 
     @Test
     void isPaymentDayShouldReturnFalseForLastNonWorkingDay(){
-
+        assertFalse(commisionalEmployee.isPaymentDay(LocalDate.of(2018, 12, 30)));
     }
 }
